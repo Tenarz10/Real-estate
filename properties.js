833c730dbab955eb6bcc984ba996e42e136421fb
Tenarz10 <afolabisamuel705@gmail.com>
2026-04-15 16:01:56 +0100

    Update properties.js

--- properties.js
+++ properties.js
@@ -3,7 +3,6 @@ const properties = [
     id: 1,
     title: "Luxury Duplex in Bodija",
     price: 95000000,
-    displayPrice: "₦95,000,000",
     location: "Bodija, Ibadan",
     image: "https://images.unsplash.com/photo-1600585154340-be6161a56a0c"
   },
@@ -11,7 +10,6 @@ const properties = [
     id: 2,
     title: "Modern Apartment in Akobo",
     price: 65000000,
-    displayPrice: "₦65,000,000",
     location: "Akobo, Ibadan",
     image: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c"
   },
@@ -19,7 +17,6 @@ const properties = [
     id: 3,
     title: "Detached House in Iyaganku",
     price: 120000000,
-    displayPrice: "₦120,000,000",
     location: "Iyaganku, Ibadan",
     image: "https://images.unsplash.com/photo-1568605114967-8130f3a36994"
   },
@@ -27,7 +24,6 @@ const properties = [
     id: 4,
     title: "Affordable Flat in Oluyole",
     price: 30000000,
-    displayPrice: "₦30,000,000",
     location: "Oluyole, Ibadan",
     image: "https://images.unsplash.com/photo-1507089947368-19c1da9775ae"
   },
@@ -35,9 +31,7 @@ const properties = [
     id: 5,
     title: "Serviced Apartment in Jericho",
     price: 80000000,
-    displayPrice: "₦80,000,000",
     location: "Jericho, Ibadan",
     image: "https://images.unsplash.com/photo-1599423300746-b62533397364"
   }
 ];
-
